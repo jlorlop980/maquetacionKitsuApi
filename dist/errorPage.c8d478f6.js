3d7062affc4846118e642fb9641e46869a97ea33
jlorlop980 <jalorenzo@iesrafaelalberti.es>
2022-12-11 00:53:49 +0100

    added 404 page for both mobile and desktop

--- dist/errorPage.c8d478f6.js
+++ dist/errorPage.c8d478f6.js
@@ -142,13 +142,13 @@
       this[globalName] = mainExports;
     }
   }
-})({"lX4BT":[function(require,module,exports) {
+})({"iD1dc":[function(require,module,exports) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
-module.bundle.HMR_BUNDLE_ID = "7055c94b59712999";
+module.bundle.HMR_BUNDLE_ID = "67740045c8d478f6";
 "use strict";
 /* global HMR_HOST, HMR_PORT, HMR_ENV_HASH, HMR_SECURE, chrome, browser, globalThis, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
 import type {
@@ -531,8 +531,8 @@ function hmrAcceptRun(bundle, id) {
     acceptedAssets[id] = true;
 }
 
-},{}],"4M6V8":[function(require,module,exports) {
+},{}],"3VODm":[function(require,module,exports) {
 
-},{}]},["lX4BT","4M6V8"], "4M6V8", "parcelRequire94c2")
+},{}]},["iD1dc","3VODm"], "3VODm", "parcelRequire94c2")
 
-//# sourceMappingURL=diseno.59712999.js.map
+//# sourceMappingURL=errorPage.c8d478f6.js.map
