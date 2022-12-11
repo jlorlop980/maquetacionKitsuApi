1e475c1922f1be2997bd0f38c777ef94ee5f8d34
jlorlop980 <jalorenzo@iesrafaelalberti.es>
2022-12-11 19:30:38 +0100

    fixed some views

--- dist/listado.bda4afae.js
+++ dist/listado.bda4afae.js
@@ -7,12 +7,16 @@ function changeAnime() {
     document.getElementById("M").className = "activado";
     document.getElementById("RA").className = "activado";
     document.getElementById("RM").className = "desactivado";
+    let anime = document.getElementById("anime").className = "main-mobile-section-anime activado";
+    let manga = document.getElementById("manga").className = "desactivado";
 }
 function changeManga() {
     document.getElementById("A").className = "activado";
     document.getElementById("M").className = "desactivado";
     document.getElementById("RA").className = "desactivado";
     document.getElementById("RM").className = "sactivado";
+    let anime = document.getElementById("anime").className = "desactivado";
+    let manga = document.getElementById("manga").className = "main-mobile-section-anime activado";
 }
 
 //# sourceMappingURL=listado.bda4afae.js.map
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -12,6 +12,8 @@ function changeAnime(){
     document.getElementById("M").className="activado";
     document.getElementById("RA").className="activado";
     document.getElementById("RM").className="desactivado";
+    let anime= document.getElementById("anime").className="main-mobile-section-anime activado";
+    let manga= document.getElementById("manga").className="desactivado";
 }
 
 function changeManga(){
@@ -19,4 +21,6 @@ function changeManga(){
     document.getElementById("M").className="desactivado";
     document.getElementById("RA").className="desactivado";
     document.getElementById("RM").className="sactivado";
+    let anime= document.getElementById("anime").className="desactivado";
+    let manga= document.getElementById("manga").className="main-mobile-section-anime activado";
 }
