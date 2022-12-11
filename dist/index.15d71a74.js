668a5838edc4ba2f422787f7e827178136328876
jlorlop980 <jalorenzo@iesrafaelalberti.es>
2022-12-11 23:18:21 +0100

    finished

---
+++ dist/index.15d71a74.js
@@ -0,0 +1,7 @@
+function ttheme() {
+    let x = document.getElementById("1");
+    if (x.className == "theme") x.className = "theme--dark";
+    else x.className = "theme";
+}
+
+//# sourceMappingURL=index.15d71a74.js.map
---
+++ src/assets/js/cambiarTema.js
@@ -0,0 +1,9 @@
+function ttheme(){
+    let x=document.getElementById("1");
+    if(x.className=="theme"){
+        x.className="theme--dark";
+    }
+    else{
+        x.className="theme";
+    }
+}
